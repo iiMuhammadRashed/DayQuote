f882f9170bf527430300b00ecb50cb31f066b667
Muhammad Rashed <47828452+iiMuhammadRashed@users.noreply.github.com>
2023-02-04 19:23:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ var Quotes = [
 function printRandomizeQuote() {
   randomIndex = Math.floor(Math.random() * Quotes.length);
   document.getElementById("showQuote").innerHTML = `
-    <div class="qoute_box w-50 mx-auto text-center shadow rounded my-5">
+    <div class="qoute_box w-50 mx-auto text-center shadow rounded my-5 px-3">
       <h2 class="my-2 pt-4 fs-2">Your Quote</h2>
       <p class="my-3 p-3 fw-bold fs-3">${Quotes[randomIndex].quote}</p>
       <p class="pb-5 fw-medium fs-4">${Quotes[randomIndex].author}</p>
